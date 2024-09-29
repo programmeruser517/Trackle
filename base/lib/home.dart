acfde00dcbd9932473a6ec80cf2ec3ef4912916e
Peter Pena <programmeruser517@gmail.com>
2024-09-28 20:00:18 -0400

    prelim home

---
+++ base/lib/home.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  // Subpages content for each tab
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(child: Text('Home')),
+    Center(child: Text('Switches')),
+    Center(child: Text('Log')),
+    Center(child: Text('Energy')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trackle'),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home), // Home Icon
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.toggle_on), // Switch Icon (replace with image if needed)
+            label: 'Switches',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.insert_chart), // Log Icon (replace with image if needed)
+            label: 'Log',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.battery_charging_full), // Energy Icon (replace with image if needed)
+            label: 'Energy',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
